de9dfc87aeb397978d1aec5e862f72f9aa1113af
Szymon Dros <dros.szymon@gmail.com>
2021-09-22 11:36:39 +0200

    mobile view of about-us section

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import "../scss/home/home-about.scss";
+import {isMobile} from "react-device-detect";
+import HomeAboutSignature from "./HomeAboutSignature";
 
 const HomeAbout = () => {
     return (
-        <div>
-            More about us
-        </div>
+        <section id="about-us" className="home__about home__page__wrapper">
+            <div className="about__description">
+                <h1>O nas</h1>
+                <hr />
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya
+                    nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <HomeAboutSignature />
+            </div>
+            {isMobile ?
+                <></>
+                :
+                <img src="/images/People.png" alt="People" />
+            }
+        </section>
     );
 };
 
